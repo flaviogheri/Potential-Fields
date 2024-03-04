392ef548aa7ca9d34e0fd06ccdda198f0f5898f0
flaviogheri <gheriarnaudflavio@gmail.com>
2024-03-04 17:48:17 +0100

    created the swarm potential field

---
+++ 2dplotswarm.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from swarmfield import Swarm_field
+
+# Define parameters
+gamma = 0.5
+center = [0, 0]
+Rt_ratio = 0.1
+
+
+# Define grid for potential field visualization
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)  # Create meshgrid for grid structure
+
+
+# Calculate force magnitudes for each grid point
+F_total = np.zeros_like(X)  # Array to store force magnitudes (scalars)
+swarm_field = Swarm_field(pos=[1, 1], gamma=gamma, center=center, Rt_ratio=Rt_ratio)
+
+
+
+for i in range(len(x)):
+    for j in range(len(y)):
+        swarm_field.pos = [x[i], y[j]]
+        F_total[j, i] = np.linalg.norm(swarm_field.velocity())
+
+
+# Create 2D colormap plot of the potential field
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Plot the magnitude
+magnitude_plot = ax.imshow(
+    F_total, cmap='viridis', extent=(x.min(), x.max(), y.min(), y.max()), origin='lower',
+    interpolation='nearest', vmin=0, vmax=5
+)
+ax.set_xlabel('X Position')
+ax.set_ylabel('Y Position')
+ax.set_title('Potential Field Visualization (Magnitude)')
+ax.set_xlim([X.min(), X.max()])
+ax.set_ylim([Y.min(), Y.max()])
+
+# Add colorbar
+plt.colorbar(magnitude_plot, ax=ax, orientation='vertical', label='Magnitude')
+
+plt.show()
--- swarmfield.py
+++ swarmfield.py
@@ -3,26 +3,82 @@ import numpy as np
 
 
 
-class Swarm():
-    def __init__():
-        self.gamma = 5
-        self.center = []
+class Swarm_field:
+    def __init__(self, pos, gamma, center = [0,0], Rt_ratio=0.1):
+
+        self.pos = [1,1]
+        self.center = center
+
+        self.gamma = gamma
+        self.gamma_rot = 5
+        self.alpha_in = 5 # should be within
+        self.alpha_out = 5 # should be within
+        self.alpha_perp = 5 # should be within
+        self.Rto_ratio = Rt_ratio
+        self.Rti_ratio = Rt_ratio
+
+        self.rad
+        self.calculate_R_star
+        self.calculate_delta_R_in
+        self.calculate_delta_R_out
+
+    @property 
+    def rad(self):
+        self.r = math.sqrt((self.pos[0] - self.center[0])**2 + ((self.gamma**2) * (self.pos[1] - self.center[1])**2))
+
+    @property
+    def calculate_R_star(self):
+        self.R_star = math.sqrt((self.pos[0] - self.center[0])**2 + self.gamma * (self.pos[1] - self.center[1])**2)
+    @property
+    def calculate_delta_R_in(self):
+        """ inside boundary of the R neighboorhood"""
+        self.delta_R_in = self.Rti_ratio * self.R_star
+
+    @property
+    def calculate_delta_R_out(self):
+        """ outside boundary of the R neighboorhood"""
+        self.delta_R_out = self.Rto_ratio * self.R_star
+
 
     #@property 
-    def bivariate_normal(self, x, y):
-        return math.e**-((x-self.center[0])**2 + self.gamma*( y - self.center[1])**2)
+    def bivariate_normal(self):
+        return math.e**-((self.pos[0]-self.center[0])**2 + self.gamma*(self.pos[1] - self.center[1])**2)
 
-    def dx(self, x, y):
-        return self.bivariate_normal(x, y) * 2 * (x - self.centre[0])
+    def dx(self):
+        return self.bivariate_normal() * 2 * (self.pos[0] - self.center[0])
     
-    def dy(self, x, y):
-        return self.bivariate_normal(x, y) * 2 * self.gamma * (y - self.centre[1])
+    def dy(self):
+        return self.bivariate_normal() * 2 * self.gamma * (self.pos[1] - self.center[1])
 
-    def xrot(self, x, y, theta):
-        return (x - self.center[0])* math.cos(theta) - (y- self.center[1]) * math.sin(theta)
+    # def xrot(self, x, y, theta):
+    #     return (x - self.center[0])* math.cos(theta) - (y- self.center[1]) * math.sin(theta)
     
-    def yrot(self, x, y, theta):
-        return (x - self.center[0])* math.sin(theta) + (y- self.center[1]) * math.cos(theta)
+    # def yrot(self, x, y, theta):
+    #     return (x - self.center[0])* math.sin(theta) + (y- self.center[1]) * math.cos(theta)
     
-    def R_star():
-        return (x - center)**2 + self.gamma * ()**2
+    def Sin(self):
+        """ Limiting function (sigmoid)-> gradient vector field directed towards outside.
+        (Called as such as it is only used within the center of the field)"""
+        return 1 - (1/(1 + math.e ** (self.alpha_in*(self.r-(self.R_star+self.delta_R_in)))))
+
+    def Sout(self):
+        """ gradient vector fielsd directed towards center"""
+        return 1 - (1/(1+ math.e ** (-self.alpha_out*(self.r - (self.R_star+self.delta_R_out)))))
+    
+    def N(self):
+        return (math.e ** (-self.alpha_perp * (self.r - self.R_star)**20))
+
+    def SGN(self):
+        return (1 - 2 * (1/(1+ math.e ** (-self.alpha_perp* self.gamma_rot))))
+
+    def velocity(self):
+        """" function that returns the velocity of swarm field depending on the agents position
+        inputs: x, y
+        """
+        vx = self.Sin() - self.Sout()*self.dx() + self.SGN() * self.N() * self.dx()
+        vy = self.Sin() - self.Sout()*self.dy() + self.SGN() * self.N() * self.dy()
+        return (vx, vy)
+
+        # print((self.Sin() - self.Sout())*(self.dx(), self.dy()) + self.SGN() * self.N() * (self.dx(), self.dy()))
+        # return None
+    
